ef22fba6667f4dcedd41eafb53497be6d6b12274
Cube <thefabulouscube@tadaatiedye.com>
2017-02-26 10:20:48 -0500

    Add project files.

---
+++ CodeProjectDecoupling/AppPoolWatcher.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeProjectDecoupling
+{
+    class AppPoolWatcher
+    {
+        EventLogWriter writer = null;
+        EmailSender email = null;
+        SMSAlert sms = null;
+
+        // Severity level :
+        // 0: Log
+        // 1: Log & Email
+        // 2: SMS, Email, & Log
+        public void Notify(string message, int level)
+        {
+            // protect against invalid severity levels
+            level = (level > 2) ? 2 : level;
+            level = (level < 0) ? 0 : level;
+            switch (level)
+            {
+                case 2:
+                    {
+                        if (sms == null)
+                        {
+                            sms = new SMSAlert();
+                        }
+                        sms.Text(message);
+                        break;
+                    }
+                case 1:
+                    {
+                        if (email == null)
+                        {
+                            email = new EmailSender();
+                        }
+                        email.Send(message);
+
+                        break;
+
+                    }
+                case 0:
+                    {
+                        if (writer == null)
+                        {
+                            writer = new EventLogWriter();
+                        }
+                        writer.Write(message);
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+            if (level >= 1)
+            {
+            }
+        }
+    }
+
+    class EventLogWriter
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine($"Log Entry:{message}");
+        }
+    }
+
+    class EmailSender
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"Email Sent:{message}");
+        }
+    }
+
+    class SMSAlert
+    {
+        public void Text(string message)
+        {
+            Console.WriteLine($" SMS Alert: {message}");
+        }
+    }
+}
---
+++ CodeProjectDecoupling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeProjectDecoupling
+{
+    class Program
+    {
+        static Random rand = new Random();
+        static AppPoolWatcher alert = new AppPoolWatcher();
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                int level = rand.Next(3);
+                alert.Notify($"Hello there!, {i}, {level}", level);
+            }
+
+
+            
+            Console.ReadKey();
+        }
+    }
+}
